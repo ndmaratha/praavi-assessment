bff62e2eb68c87a1150ef622bdb811a9fbb2e676
Nayan Desai <ndmaratha01@gmail.com>
2025-02-19 05:15:40 +0530

    Setup Done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div className='text-3xl text-yellow-300 font-black font-bold'>
+				hello shree Radha
+			</div>
+		</>
+	);
+}
+
+export default App;
